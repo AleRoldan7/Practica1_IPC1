641f93878df61c0cef01bc4c532ecd1530bb8968
AleRoldan7 <crisalej.1115@gmail.com>
2025-08-02 12:33:25 -0600

    Primer

---
+++ src/main/java/com/mycompany/practica_ipc2/Practica_IPC2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practica_ipc2;
+
+/**
+ *
+ * @author alejandro
+ */
+public class Practica_IPC2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
